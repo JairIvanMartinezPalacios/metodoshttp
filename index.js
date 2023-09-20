c00c5deba33e81eafe26bd216f5ce02071d32a4e
Jair Ivan Martinez Palacios <l20350292@ittux.edu.mx>
2023-09-20 15:16:10 -0600

    Query params

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 const express = require('express')
 
 const app = express()
+/*
 app.get('/', (request, response)=> {
-    response.send('Neto is gay')
-
+    response.send('Hello world from Express!')
 })
+*/
+
+/* METODOS CON SEND PARA GET, POST, PUT, PATCH Y DELETE */
+ss
+/*
+    app.get('/',(request, response)=>{
+        response.send({msg: 'Hello GET'})
+})
+
+app.post('/',(request, response)=>{
+    response.send({msg: 'Hello POST'})
+})
+
+app.put('/',(request, response)=>{
+    response.send({msg: 'Hello PUT'})
+})
+
+app.patch('/',(request, response)=>{
+    response.send({msg: 'Hello PATCH'})
+})
+
+app.delete('/',(request, response)=>{
+    response.send({msg: 'Hello DELETE'})
+})
+*/
+
+
+/* METODOS CON json PARA GET, POST, PUT, PATCH Y DELETE */
+
+app.get('/',(req, res)=>{
+    res.json({msg: 'Hello GET'})
+})
+
+app.post('/',(req, res)=>{
+res.json({msg: 'Hello POST'})
+})
+
+app.put('/',(req, res)=>{
+res.jspn({msg: 'Hello PUT'})
+})
+
+app.patch('/',(req, res)=>{
+res.json({msg: 'Hello PATCH'})
+})
+
+app.delete('/',(req, res)=>{
+res.json({msg: 'Hello DELETE'})
+})
+
+
 /*
 *
 *
 *     FUNCIONALIDAD
 *
 */
-//http://localhost:3000/
+/*http://localhost:3000/ */
 
 app.listen(3000,()=>{
     console.log('listening on port 3000')
